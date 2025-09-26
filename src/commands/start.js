bcdfb2545f0f9b7f590731ca62d78fc839de7ba8
HDan9304 <101402467+HDan9304@users.noreply.github.com>
2025-09-26 22:53:54 +0800

    Update start.js

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,19 +1,13 @@
 import { send } from "../utils/telegram.js";
-import { ascii, esc, B, startLink } from "../utils/fmt.js";
+import { ascii, esc, B } from "../utils/fmt.js";
 
 export default async function start(env, chatId, msg) {
   const first = ascii((msg?.from?.first_name || "there").trim());
-  const tapStart = startLink(env) || "<code>/start</code>"; // deep link if BOT_USERNAME exists
-
   const html = [
     `<b>${esc(`Hey ${first}!`)}</b>`,
     "",
-    `${B("This bot is alive")}`,
-    `Right now I only support ${tapStart}.`,
-    "",
-    `${B("Tip")}`,
-    `You can tap ${tapStart} to run it again.`
+    `${B("Link your team")}`,
+    `Use <code>/link &lt;team_id&gt;</code> (find it in the FPL URL like <code>/entry/1234567/</code>).`
   ].join("\n");
-
   await send(env, chatId, html, "HTML");
 }
