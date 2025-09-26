fc33407ce53283be2c91496c67155c88b91227ae
HDan9304 <101402467+HDan9304@users.noreply.github.com>
2025-09-26 22:55:37 +0800

    Update telegram.js

--- src/utils/telegram.js
+++ src/utils/telegram.js
@@ -1,7 +1,6 @@
 export async function send(env, chat_id, text, parse_mode = null) {
   const payload = { chat_id, text, disable_web_page_preview: true };
   if (parse_mode) payload.parse_mode = parse_mode;
-
   try {
     const r = await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
       method: "POST",
